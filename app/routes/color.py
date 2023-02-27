74ff387bf76a1033c50ac13ce8b9cd9127da7f6b
diegoitaliait <diego.lagosmorales@pagopa.it>
2023-02-27 22:42:39 +0100

    added dapr sdk request

--- app/routes/color.py
+++ app/routes/color.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter
 import requests
 import logging
+from dapr.clients import DaprClient
 
 color = APIRouter()
 
@@ -11,14 +12,26 @@ async def home():
 
 
 @color.get("/dapr/http")
-async def random_color():
+async def dapr_http_color():
     """
     return random color with a raw http request to dapr
     """
     r = requests.get('http://localhost:3500/v1.0/invoke/backend/method/color')
     color = r.text
     logging.info(r)
-    return {"random color": f'${color}'}
+    return {"random color": f'{color}'}
+
+
+@color.get("/dapr/sdk")
+async def dapr_sdk_color():
+    """
+    return random color with a sdk request to dapr
+    """
+    with DaprClient() as d:
+        r = d.invoke_method('backend', 'color')
+        color = r
+        logging.info(r)
+        return {"random color": f'{color}'}
 
 
 
