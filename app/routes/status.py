d0115ebd66c5411d957874bd9a41d8d625164d29
diegoitaliait <diego.lagosmorales@pagopa.it>
2023-02-27 23:08:20 +0100

    status now is empty with 200 ok

--- app/routes/status.py
+++ app/routes/status.py
@@ -1,8 +1,9 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Response
+from http import HTTPStatus
 
 status_route = APIRouter()
 
 
 @status_route.get("/")
 async def status():
-    return {"status": "ok"}
+    return Response(status_code=HTTPStatus.OK)
