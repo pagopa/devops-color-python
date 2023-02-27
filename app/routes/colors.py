66ba4403bebf8b9e33746a73ee682f125a194e47
diegoitaliait <diego.lagosmorales@pagopa.it>
2023-02-27 14:35:43 +0100

    added color call to http darp

--- app/routes/colors.py
+++ app/routes/colors.py
@@ -1,4 +1,6 @@
 from fastapi import APIRouter
+import requests
+import logging
 
 colors = APIRouter()
 
@@ -8,9 +10,14 @@ async def home():
     return {"message": "home of colors"}
 
 
-@colors.get("/random")
+@colors.get("/dapr/http")
 async def random_color():
     """
     return random color
     """
-    return {"random color": "ok"}
+    r = requests.get('http://localhost:3500/v1.0/invoke/backend/method/color')
+    logging.info(r)
+    return {"random color": f'${r}'}
+
+
+
